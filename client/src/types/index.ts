3a1eb4251b6d8a8182c62e6a3cd2d7df56114fa3
HirunaD <de_silva_pkhd_e22@engug.ruh.ac.lk>
2026-02-12 08:56:50 +0530

    feat: create types

---
+++ client/src/types/index.ts
@@ -0,0 +1,18 @@
+export type Priority = "Low" | "Medium" | "High";
+export type Status = "Open" | "In Progress" | "Resolved" | "Closed";
+
+export interface Issue {
+  _id: string;
+  title: string;
+  description: string;
+  status: Status;
+  priority: Priority;
+  severity?: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface IssueStats {
+  _id: Status;
+  count: number;
+}
